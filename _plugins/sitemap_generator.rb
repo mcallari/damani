b583739777c08b4669f283dd4d9c6fee6fddac24
Mike Callari <mike.callari@gmail.com>
2012-12-18 16:30:17 -0500

    Updating site url.

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -39,7 +39,7 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://mcallari.github.com/damani"
+  MY_URL = "http://mcallari.github.com/damani/"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
